1b79ce66bb4ad4a1a21a8f15557e73d905ddeb5f
Min Khati <meetup.min@gmail.com>
2018-03-13 11:45:55 -0400

    Update server.js file

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(port);
-console.log('Server started');
+console.log('Server Started');
